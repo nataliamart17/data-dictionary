e67e3bf101fedda58fbcebda7b69850a2dc26733
nataliamart17 <nataliarammart3@gmail.com>
2024-09-20 10:23:25 -0600

    Update dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -3,7 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+ typedef struct node
+{
+  char value[50];
+  //in-memory
 #define EMPTY_POINTER -1
 
 int initializeDataDictionary(const char *dictionaryName);
